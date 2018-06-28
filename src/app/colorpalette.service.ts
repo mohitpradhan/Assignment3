af414eb0f4fa7abb037e3686c4a3f88965053107
Mohit <mohitpradhan310@gmail.com>
2018-06-28 17:08:21 -0400

    Method returning all data in service added

--- src/app/colorpalette.service.ts
+++ src/app/colorpalette.service.ts
@@ -1,10 +1,34 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import {Colorpalette} from './colorpalette';
+import {Observable, of, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorpaletteService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  private handleError(error: HttpErrorResponse) {
+
+    // client-side or network error
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred: ', error.error.message);
+      // additional action to handle error goes here:
+    } else { // backend error
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+      // additional action to handle error goes here:
+    }
+
+    return throwError ('Something bad happened!');
+  }
+
+  getColorPalettes():Observable<Colorpalette[]>{
+    return this.http.get<Colorpalette[]>('http://api.tkusaka.com/palette_api.php?api_key=5b3549186e29f');
+  }
 }
+
